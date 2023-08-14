0fdb4a57cb182b156e85cd4f89d0cfdd2c8196b2
numonce <numonce@protonmail.com>
2023-08-14 13:46:22 -0400

    Adds the ability to specify the number of threads and naming cloned VMs.

--- src/main.rs
+++ src/main.rs
@@ -32,93 +32,6 @@ struct JobData {
 struct Job {
     exitstatus: String,
 }
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let app = Command::new("terrabad")
-        .arg(
-            Arg::new("Url")
-                .long("url")
-                .short('U')
-                .help("url of the Proxmox host")
-                .required(true),
-        )
-        .arg(
-            Arg::new("Username")
-                .long("user")
-                .short('u')
-                .help("Username for proxmox auth")
-                .required(true),
-        )
-        .arg(
-            Arg::new("Password")
-                .long("password")
-                .short('p')
-                .help("Password for proxmox auth")
-                .required(true),
-        )
-        .arg(
-            Arg::new("Action")
-                .long("Action")
-                .short('a')
-                .help("clone, etc...")
-                .required(true)
-                .value_parser(["clone", "destroy", "bulk_clone", "bulk_destroy"]),
-        )
-        .arg(
-            Arg::new("Name")
-                .long("name")
-                .short('n')
-                .required(true)
-                .help("Name of the node"),
-        )
-        .arg(
-            Arg::new("Source")
-                .long("source")
-                .short('s')
-                .help("Source template VMID for action."),
-        )
-        .arg(
-            Arg::new("Destination")
-                .long("destination")
-                .short('d')
-                .requires("Action")
-                .help("Destination template VMID for action."),
-        )
-        .arg(
-            Arg::new("Min")
-                .long("min")
-                .short('m')
-                .requires("Action")
-                .help("First VMID for range. Needed for bulk actions.")
-                .required_if_eq_any([("Action", "bulk_clone")]),
-        )
-        .arg(
-            Arg::new("Max")
-                .long("max")
-                .short('M')
-                .requires("Action")
-                .help("Last VMID for range. Needed for bulk actions.")
-                .required_if_eq_any([("Action", "bulk_clone")]),
-        )
-        .arg(
-            Arg::new("Clone_type")
-                .long("clone_type")
-                .short('t')
-                .help("Type of clone. Can either be linked or full.")
-                .default_value("linked")
-                .value_parser(["linked", "full"]),
-        )
-        .get_matches();
-    match app.get_one::<String>("Action").unwrap().as_str() {
-        "clone" => create_clone(app).await?,
-        "destroy" => destroy_vm(app).await?,
-        "bulk_clone" => bulk_clone(app).await?,
-        "bulk_destroy" => bulk_destroy(app).await?,
-        _ => panic!("asdfasdf"),
-    }
-    Ok(())
-}
-
 async fn get_token(
     username: &mut String,
     password: &String,
@@ -148,7 +61,7 @@ async fn get_token(
 }
 
 async fn create_clone(app: ArgMatches) -> Result<(), Box<dyn Error>> {
-    let name = app.get_one::<String>("Name").unwrap();
+    let nodename = app.get_one::<String>("Node").unwrap();
     let dst = app.get_one::<String>("Destination").unwrap();
     let src = app.get_one::<String>("Source");
     let url = app.get_one::<String>("Url").unwrap();
@@ -156,6 +69,12 @@ async fn create_clone(app: ArgMatches) -> Result<(), Box<dyn Error>> {
     let username = app.get_one::<String>("Username").unwrap();
     let password = app.get_one::<String>("Password").unwrap();
     let token = get_token(&mut username.clone(), password, url).await?;
+    let name = app.get_one::<String>("Name");
+
+    let name = match name {
+        Some(n) => n,
+        None => "",
+    };
 
     let src = match src {
         Some(e) => e,
@@ -177,14 +96,17 @@ async fn create_clone(app: ArgMatches) -> Result<(), Box<dyn Error>> {
         HeaderValue::from_str(token.data.csrf.as_str())?,
     );
     let mut json_data = Map::new();
+    if name != "" {
+        json_data.insert("name".to_string(), Value::String(name.to_owned()));
+    }
     json_data.insert("newid".to_string(), Value::String(dst.to_owned()));
-    json_data.insert("node".to_string(), Value::String(name.to_owned()));
+    json_data.insert("node".to_string(), Value::String(nodename.to_owned()));
     json_data.insert(
         "vmid".to_string(),
         serde_json::Value::String(src.to_owned()),
     );
     json_data.insert("full".to_string(), Value::Bool(full));
-    let n_url = format!("{}/api2/json/nodes/{}/qemu/{}/clone", url, name, src);
+    let n_url = format!("{}/api2/json/nodes/{}/qemu/{}/clone", url, nodename, src);
     let text = client
         .post(n_url)
         .headers(headers.clone())
@@ -194,7 +116,7 @@ async fn create_clone(app: ArgMatches) -> Result<(), Box<dyn Error>> {
         .text()
         .await?;
     let upid: UPIDData = serde_json::de::from_str::<UPIDData>(text.as_str())?;
-    finished(headers, upid, url, name).await?;
+    finished(headers, upid, url, nodename).await?;
     Ok(())
 }
 
@@ -202,7 +124,7 @@ async fn destroy_vm(app: ArgMatches) -> Result<(), Box<dyn Error>> {
     let client = ClientBuilder::new()
         .danger_accept_invalid_certs(true)
         .build()?;
-    let name = app.get_one::<String>("Name").unwrap();
+    let name = app.get_one::<String>("Node").unwrap();
     let src = app.get_one::<String>("Source");
     let url = app.get_one::<String>("Url").unwrap();
     let username = app.get_one::<String>("Username").unwrap();
@@ -234,12 +156,18 @@ async fn destroy_vm(app: ArgMatches) -> Result<(), Box<dyn Error>> {
 async fn bulk_clone(app: ArgMatches) -> Result<(), Box<dyn Error>> {
     let max = app.get_one::<String>("Max").unwrap().parse::<i32>()?;
     let min = app.get_one::<String>("Min").unwrap().parse::<i32>()?;
-    let name = app.get_one::<String>("Name").unwrap();
+    let nodename = app.get_one::<String>("Node").unwrap();
     let src = app.get_one::<String>("Source");
     let url = app.get_one::<String>("Url").unwrap();
     let username = app.get_one::<String>("Username").unwrap();
     let password = app.get_one::<String>("Password").unwrap();
     let token = get_token(&mut username.clone(), password, url).await?;
+    let name = app.get_one::<String>("Name");
+
+    let name = match name {
+        Some(n) => n,
+        None => "",
+    };
     let src = match src {
         Some(e) => e,
         None => panic!("The argument requires a source VMID"),
@@ -249,7 +177,7 @@ async fn bulk_clone(app: ArgMatches) -> Result<(), Box<dyn Error>> {
         "full" => true,
         _ => false,
     };
-    let n_url = format!("{}/api2/json/nodes/{}/qemu/{}/clone", url, name, src);
+    let n_url = format!("{}/api2/json/nodes/{}/qemu/{}/clone", url, nodename, src);
     let client = reqwest::ClientBuilder::new()
         .danger_accept_invalid_certs(true)
         .build()?;
@@ -261,21 +189,27 @@ async fn bulk_clone(app: ArgMatches) -> Result<(), Box<dyn Error>> {
         HeaderValue::from_str(token.data.csrf.as_str())?,
     );
 
-    let semaphore = Arc::new(Semaphore::new(2));
+    let semaphore = Arc::new(Semaphore::new(
+        app.get_one::<String>("Threads").unwrap().parse::<usize>()?,
+    ));
     let jobs: Vec<_> = (min..max + 1).collect();
     let tasks: Vec<_> = jobs
         .into_iter()
         .map(|newid| {
             let permit = semaphore.clone();
             let mut json_data = Map::new();
+            if name != "" {
+                let temp_name = format!("{}{}", name, (newid - min));
+                json_data.insert(String::from("name"), Value::String(temp_name));
+            }
             json_data.insert(String::from("newid"), Value::String(newid.to_string()));
-            json_data.insert(String::from("node"), Value::String(name.clone()));
+            json_data.insert(String::from("node"), Value::String(nodename.clone()));
             json_data.insert(String::from("vmid"), Value::String(src.clone()));
             json_data.insert(String::from("full"), Value::Bool(full.clone()));
             let url = url.clone();
             let client = client.clone();
             let src = src.clone();
-            let name = name.clone();
+            let name = nodename.clone();
             let n_url = n_url.clone();
             let headers = headers.clone();
             tokio::spawn(async move {
@@ -312,7 +246,7 @@ async fn bulk_destroy(app: ArgMatches) -> Result<(), Box<dyn Error>> {
     let client = ClientBuilder::new()
         .danger_accept_invalid_certs(true)
         .build()?;
-    let name = app.get_one::<String>("Name").unwrap();
+    let name = app.get_one::<String>("Node").unwrap();
     let url = app.get_one::<String>("Url").unwrap();
     let username = app.get_one::<String>("Username").unwrap();
     let password = app.get_one::<String>("Password").unwrap();
@@ -325,7 +259,9 @@ async fn bulk_destroy(app: ArgMatches) -> Result<(), Box<dyn Error>> {
         HeaderValue::from_str(token.data.csrf.as_str())?,
     );
 
-    let semaphore = Arc::new(Semaphore::new(2));
+    let semaphore = Arc::new(Semaphore::new(
+        app.get_one::<String>("Threads").unwrap().parse::<usize>()?,
+    ));
     let jobs: Vec<_> = (min..max + 1).collect();
     let tasks: Vec<_> = jobs
         .into_iter()
@@ -352,6 +288,7 @@ async fn bulk_destroy(app: ArgMatches) -> Result<(), Box<dyn Error>> {
                     let text = request.text().await.unwrap(); // Same with the aformetioned comment
                     let upid = serde_json::de::from_str::<UPIDData>(text.as_str()).unwrap();
                     finished(headers.clone(), upid, &url, &name).await.unwrap();
+                    println!("{} destroyed", newid);
                 } else {
                     println!(
                         "Server returned: {}. Make sure {} is valid.",
@@ -400,5 +337,104 @@ async fn finished(
         }
     }
 
+    Ok(())
+}
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let app = Command::new("terrabad")
+        .arg(
+            Arg::new("Url")
+                .long("url")
+                .short('U')
+                .help("url of the Proxmox host")
+                .required(true),
+        )
+        .arg(
+            Arg::new("Username")
+                .long("user")
+                .short('u')
+                .help("Username for proxmox auth")
+                .required(true),
+        )
+        .arg(
+            Arg::new("Password")
+                .long("password")
+                .short('p')
+                .help("Password for proxmox auth")
+                .required(true),
+        )
+        .arg(
+            Arg::new("Action")
+                .long("action")
+                .short('a')
+                .help("clone, etc...")
+                .required(true)
+                .value_parser(["clone", "destroy", "bulk_clone", "bulk_destroy"]),
+        )
+        .arg(
+            Arg::new("Name")
+                .long("name")
+                .short('n')
+                .help("Desired name of the created VM. For bulk actions this will add a number."),
+        )
+        .arg(
+            Arg::new("Node")
+                .long("node")
+                .short('N')
+                .required(true)
+                .help("Node of the node"),
+        )
+        .arg(
+            Arg::new("Source")
+                .long("source")
+                .short('s')
+                .help("Source template VMID for action."),
+        )
+        .arg(
+            Arg::new("Destination")
+                .long("destination")
+                .short('d')
+                .requires("Action")
+                .help("Destination template VMID for action."),
+        )
+        .arg(
+            Arg::new("Min")
+                .long("min")
+                .short('m')
+                .requires("Action")
+                .help("First VMID for range. Needed for bulk actions.")
+                .required_if_eq_any([("Action", "bulk_clone")]),
+        )
+        .arg(
+            Arg::new("Max")
+                .long("max")
+                .short('M')
+                .requires("Action")
+                .help("Last VMID for range. Needed for bulk actions.")
+                .required_if_eq_any([("Action", "bulk_clone")]),
+        )
+        .arg(
+            Arg::new("Clone_type")
+                .long("clone_type")
+                .short('T')
+                .help("Type of clone. Can either be linked or full.")
+                .default_value("linked")
+                .value_parser(["linked", "full"]),
+        )
+        .arg(
+            Arg::new("Threads")
+                .long("threads")
+                .short('t')
+                .help("Number of workers.")
+                .default_value("3"),
+        )
+        .get_matches();
+    match app.get_one::<String>("Action").unwrap().as_str() {
+        "clone" => create_clone(app).await?,
+        "destroy" => destroy_vm(app).await?,
+        "bulk_clone" => bulk_clone(app).await?,
+        "bulk_destroy" => bulk_destroy(app).await?,
+        _ => panic!("asdfasdf"),
+    }
     Ok(())
 }
